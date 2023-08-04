9573d254966f2fde2561d5ccfcb5611391d050c0
Vilson Apostolovski II <vilsonapo@gmail.com>
2023-08-04 15:18:55 -0400

    Completed generateMarkdown function and license functions

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,18 +1,71 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license !== "None"){
+    return `![GitHub license](https://img.shields.io/badge/license-${license}-blue.svg)`;
+  }
+  return "";
+}
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license !== "None") {
+    return `\n\n[License](https://opensource.org/licenses/${license})\n`;
+  }
+  return "";
+}
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if(license !== "None"){
+    return `## License
+
+This project is licensed under the ${license} license.`;
+  }
+  return '';
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+  
+${renderLicenseBadge(data.license)}
+
+## Description
+${data.description}
+
+## Table of Contents
+- [Author](#author)
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Author
+${data.author}
+
+## Installation
+To install necessary dependencies, run the following command(s):
+\`\`\`
+${data.installation}
+\`\`\`
+
+## Usage
+${data.usage}
+
+${renderLicenseLink(data.license)}
+${renderLicenseSection(data.license)}
+
+## Contributing
+${data.contribution}
+
+## Tests
+To run tests, run the following command:
+\`\`\`
+${data.tests}
+\`\`\`
+
+## Questions
+If you have any questions about the repo, open an issue or contact me directly at ${data.email}. You can find more of my work at [${data.username}](${data.githubURL}).
+
 
 `;
 }
